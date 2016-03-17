673090872312d482a22550689148615c468201fe
Binay Mishra <binaymishramca@gmail.com>
2016-03-17 16:24:55 +0530

    Comment added to BookService.java

--- src/main/java/com/book/service/BookService.java
+++ src/main/java/com/book/service/BookService.java
@@ -2,16 +2,36 @@ package com.book.service;
 
 import java.util.List;
 
+/**
+ * @author Binay Mishra
+ *
+ */
 public interface BookService {
 
+  /**
+   * @return
+   */
   public List<Book> findAllBooks();
 
+  /**
+   * @param id
+   * @return
+   */
   public Book findBookById(final int id);
 
+  /**
+   * @param book
+   */
   public void createBook(final Book book);
 
+  /**
+   * @param book
+   */
   public void updateBook(final Book book);
 
+  /**
+   * @param id
+   */
   public void removeBookById(final int id);
 
 }
